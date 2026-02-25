4b3fce8f128e5527882bc24da0aefa4f45b47991
Sudheer Test <sudheershukla7065@gmail.com>
2026-02-25 23:21:24 +0530

    fixed done as per review

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,7 +1,13 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
 	int i =20; 
-
+	private By name = By.name("sudheer"); 
+	
+	public void getname() {
+		System.out.println("name");
+	}
 }
